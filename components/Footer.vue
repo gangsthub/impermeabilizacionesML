dd2f8bdfb836cad4ce1f7e218a85105de27e9611
Paul Melero <paulmelero@gmail.com>
2021-10-02 15:14:27 +0200

    footer++. typo

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,8 +2,8 @@
   <v-footer app absolute color="primary" ligth padless>
     <v-col class="text-center white--text" cols="12">
       <span
-        >&copy; {{ $config.BRAND_NAME }} - {{ new Date().getFullYear() }}</span
-      >
+        >{{ new Date().getFullYear() }} - {{ $config.BRAND_NAME }} &copy;
+      </span>
     </v-col>
   </v-footer>
 </template>
