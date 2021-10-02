b2ab67abfc2be35fe325a2772f6b01c1f7c01716
Paul Melero <paulmelero@gmail.com>
2021-10-02 15:43:01 +0200

    footer++

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <v-footer color="primary" ligth padless class="overflow-hidden">
     <v-row>
-      <v-col class="d-flex flex-column pa-6 mt-4">
+      <v-col class="d-flex flex-column pa-6 mt-4" cols="12" sm="12" md="6">
         <v-btn
           v-for="item in menu"
           :key="item.title"
@@ -16,7 +16,7 @@
           >{{ item.title }}</v-btn
         >
       </v-col>
-      <v-col>
+      <v-col cols="12" sm="12" md="6">
         <v-card tile flat color="transparent" class="text-center w100p">
           <v-card-text class="white--text pt-0 text-center">
             <p class="text-h5 text-center pt-10">Teléfonos</p>
