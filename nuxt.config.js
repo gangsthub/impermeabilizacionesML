f526ffc9c7c9967ca7e9614a7a551c081d0540c1
Paul Melero <paulmelero@gmail.com>
2021-10-23 18:34:37 +0200

    descripcion

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,7 +38,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: BRAND_DESCRIPTION },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
