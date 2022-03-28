65952350fd1b1ac5a597dc24249aa893533fbf14
Paul Melero <paulmelero@gmail.com>
2022-03-28 10:31:36 +0200

    fix: places title

--- pages/impermeabilizaciones/_place.vue
+++ pages/impermeabilizaciones/_place.vue
@@ -1,33 +1,34 @@
 <template>
   <div>
     <JumboSecondary
-      :title="worksByPlace[0].place"
-      :subtitle="'Impermeabilizaciones en ' + worksByPlace[0].place"
+      :title="palceTitle"
+      :subtitle="'Impermeabilizaciones en ' + palceTitle"
       min-height="10vh"
     >
     </JumboSecondary>
     <Container class="pb-16 md:px-5">
-      <v-row
-        v-for="(work, i) of worksByPlace"
-        :key="i"
-        justify="space-around"
-        class="flex-nowrap flex-column flex-md-row"
-      >
-        <div class="flex pa-3">
-          <v-avatar tile size="300">
-            <CardImage :src="work.thumbnail" width="300" height="300" />
-          </v-avatar>
-        </div>
-        <v-col cols="12" sm="9" lg="10" xs="12" class="pl-md-5">
-          <h3 class="text-h4 mb-2 primary--text">
-            {{ work.title }}
-          </h3>
-          <h4 class="overline py-0 secondary--text mb-5">
-            {{ work.service }}
-          </h4>
-          <p class="texts">{{ work.seoDescription }}</p></v-col
+      <div v-for="(work, i) of worksByPlace" :key="i">
+        <v-row
+          justify="space-around"
+          class="flex-nowrap flex-column flex-md-row"
         >
-      </v-row>
+          <div class="flex pa-3">
+            <v-avatar tile size="300">
+              <CardImage :src="work.thumbnail" width="300" height="300" />
+            </v-avatar>
+          </div>
+          <v-col cols="12" sm="9" lg="10" xs="12" class="pl-md-5">
+            <h3 class="text-h4 mb-2 primary--text">
+              {{ work.title }}
+            </h3>
+            <h4 class="overline py-0 secondary--text mb-5">
+              {{ work.service }}
+            </h4>
+            <p class="texts">{{ work.seoDescription }}</p></v-col
+          >
+        </v-row>
+        <v-divider v-if="i !== worksByPlace.length - 1" dark></v-divider>
+      </div>
     </Container>
   </div>
 </template>
@@ -50,15 +51,20 @@ export default {
   },
   head() {
     return {
-      title: `Impermeabilizaciones en ${this.place}`,
+      title: `Impermeabilizaciones en ${palceTitle}`,
       meta: [
         {
           hid: 'description',
           name: 'description',
-          content: `Últimos trabajos realizados en ${this.place}. Impermeabilizaciones, cubiertas, pisos, paredes, techos, entre otros.`,
+          content: `Últimos trabajos realizados en ${palceTitle}. Impermeabilizaciones, cubiertas, pisos, paredes, techos, entre otros.`,
         },
       ],
     }
+  },
+  computed: {
+    palceTitle() {
+      return this.worksByPlace[0]?.place || ''
+    },
   },
   created() {
     this.worksByPlace = Array.from(getWorks())
